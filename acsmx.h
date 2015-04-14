fa7c806bd85aa720f233ca5f3d9ed7e9b2a4add7
ZhangXiaoWei <zhangxiaoweihapply@qq.com>
2015-04-14 19:00:24 +0800

    acbm算法

---
+++ acsmx.h
@@ -0,0 +1,131 @@
+/* $Id$ */
+/*
+ ** Copyright (C) 2002-2008 Sourcefire, Inc.
+ ** Copyright (C) 2002 Martin Roesch <roesch@sourcefire.com>
+ **
+ ** This program is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License Version 2 as
+ ** published by the Free Software Foundation.  You may not use, modify or
+ ** distribute this program under any other version of the GNU General
+ ** Public License.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program; if not, write to the Free Software
+ ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+/*
+ **   ACSMX.H 
+ **
+ **
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef _ACSMX_H
+#define _ACSMX_H
+
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+/*
+ *   Prototypes
+ */
+
+
+#define ALPHABET_SIZE    256     
+
+#define ACSM_FAIL_STATE   -1     
+
+
+typedef struct _acsm_pattern {      
+	struct  _acsm_pattern *next;
+	unsigned char         *patrn;
+	unsigned char         *casepatrn;
+	int      n;
+	int      nocase;
+	int      offset;
+	int      depth;
+	void   * id;
+	int      iid;
+#ifdef DETECTION_OPTION_TREE
+	void   * rule_option_tree;
+#endif
+
+} ACSM_PATTERN;
+
+
+typedef struct  {    
+	/* Next state - based on input character */
+	int      NextState[ ALPHABET_SIZE ];  
+
+	/* Failure state - used while building NFA & DFA  */
+	int      FailState;   
+
+	/* List of patterns that end here, if any */
+	ACSM_PATTERN *MatchList;   
+
+}ACSM_STATETABLE; 
+
+
+/*
+ * State machine Struct
+ */
+typedef struct {
+
+	int acsmMaxStates;  
+	int acsmNumStates;  
+
+	ACSM_PATTERN    * acsmPatterns;
+	ACSM_STATETABLE * acsmStateTable;
+
+	int   bcSize;
+	short bcShift[256];
+
+}ACSM_STRUCT;
+
+/*
+ *   Prototypes
+ */
+ACSM_STRUCT * acsmNew (void);
+
+//acsmAddPattern函数中倒数第二个参数就是 Match 函数中的 id 参数
+//所以 Match函数 中 id 的实际数据类型就是 acsmAddPattern 的倒数第二个参数的实际数据类型
+//其中 Match 函数原型为 int Match(void *id, int index, void *data);
+// Match 函数的 index 参数表示匹配的特征内容（即 id 参数）在被查找的内容中所在的位置，从0开始编号
+//例如：被查找的内容为 "abcdefg" 匹配的特征内容为 "de" 则此时 index 为 3
+int acsmAddPattern( ACSM_STRUCT * p, unsigned char * pat, int n, int nocase, int offset, int depth, void *  id, int iid );
+
+int acsmCompile ( ACSM_STRUCT * acsm
+#ifdef DETECTION_OPTION_TREE
+		, int (*build_tree)(void * id, void **existing_tree)
+#endif
+		);
+
+//acsmSearch函数的倒数第二个参数就是 Match 函数中的 data 参数
+//所以 Match函数 中 data 的实际数据类型就是 acsmSearch 的倒数第二个参数的实际数据类型
+int acsmSearch ( ACSM_STRUCT * acsm,unsigned char * T, int n, 
+#ifdef DETECTION_OPTION_TREE
+		int (*Match)(void * id, void *tree, int index, void *data),
+#else
+		int (*Match)( void * id, int index, void * data ),
+#endif
+		void * data, int* current_state );
+
+void acsmFree ( ACSM_STRUCT * acsm );
+
+int acsmPrintDetailInfo(ACSM_STRUCT *);
+
+int acsmPrintSummaryInfo(void);
+
+#endif
---
+++ test_acbm.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "acsmx.h"
+
+
+int MatchFound(void *id, int index, void *data){
+    printf("%s\n", (char *) id);
+    printf("%d\n",index);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int start_state = 0;
+    ACSM_STRUCT * acsm;
+    int i, nocase = 0;
+    char text[50];
+
+    if (argc < 3){
+        printf("Usage: acsm ToBeSearchedPattern keyword-1 keyword-2 ... keyword-n -nocase\n");
+        exit(0);
+    }
+    
+    acsm = acsmNew();
+    strcpy(text, argv[1]);
+    for (i = 1; i < argc; i++){
+        if (strcmp (argv[i], "-nocase") == 0){
+            nocase = 1;
+        }
+    }
+    for (i = 2; i < argc; i++){
+        if (argv[i][0] == '-')
+            continue;
+        //acsmAddPattern函数中倒数第二个参数就是 MatchFound 函数中的 id 参数，所以 MatchFound 中 id 的实际数据类型就是 acsmAddPattern 的倒数第二个参数的实际数据类型
+        acsmAddPattern (acsm, argv[i], strlen(argv[i]), nocase, 0, 0, argv[i], i - 2);
+    }
+    acsmCompile (acsm);
+    //acsmSearch函数的倒数第二个参数就是 MatchFound 函数中的 data 参数，所以 MatchFound 中 data 的实际数据类型就是 acsmSearch 的倒数第二个参数的实际数据类型
+    acsmSearch(acsm, text, strlen(text), MatchFound, (void *) 0, &start_state);
+    acsmFree(acsm);
+    return 0;
+}
